b9a49526feaf7c0dd5a690be594a1ea381e52d8f
v-t-p <46879913+v-t-p@users.noreply.github.com>
2019-01-22 19:24:29 +1000

    Update startvm.sh

--- startvm.sh
+++ startvm.sh
@@ -5,7 +5,7 @@ OPTS="$OPTS -pidfile /tmp/windows-10-pro.pid"
 
 # Processor
 OPTS="$OPTS -cpu host,kvm=off"
-OPTS="$OPTS -smp 8,sockets=1,cores=4,threads=2"
+OPTS="$OPTS -smp 4,sockets=1,cores=2,threads=2"
 OPTS="$OPTS -enable-kvm"
 
 
@@ -20,7 +20,7 @@ OPTS="$OPTS -global PIIX4_PM.disable_s3=0"
 
 
 # Memory
-OPTS="$OPTS -m 16G"
+OPTS="$OPTS -m 8G"
 OPTS="$OPTS -mem-path /dev/hugepages"
 OPTS="$OPTS -mem-prealloc"
 OPTS="$OPTS -balloon none"
@@ -37,8 +37,8 @@ OPTS="$OPTS -soundhw hda"
 
 
 # Graphic card passthrough (Gigabyte GeForce GTX 980 G1 Gaming)
-OPTS="$OPTS -device vfio-pci,host=01:00.0,multifunction=on"
-OPTS="$OPTS -device vfio-pci,host=01:00.1"
+#OPTS="$OPTS -device vfio-pci,host=01:00.0,multifunction=on"
+#OPTS="$OPTS -device vfio-pci,host=01:00.1"
 
 
 # USB 3.0 passthrough (NEC/Renesas)
@@ -58,35 +58,35 @@ OPTS="$OPTS -boot order=c"
 
 
 # OVMF
-OPTS="$OPTS -drive if=pflash,format=raw,readonly,file=/data/machines/windows-10-pro/ovmf/OVMF_CODE-pure-efi.fd"
-OPTS="$OPTS -drive if=pflash,format=raw,file=/data/machines/windows-10-pro/ovmf/OVMF_VARS-pure-efi.fd"
+OPTS="$OPTS -drive if=pflash,format=raw,readonly,file=/usr/share/OVMF/OVMF_CODE.fd"
+OPTS="$OPTS -drive if=pflash,format=raw,file=/usr/share/OVMF/OVMF_VARS.fd"
 
 
 # System drive
-OPTS="$OPTS -drive id=disk0,if=none,cache=unsafe,format=raw,file=/data/machines/windows-10-pro/disks/disk0-system.img"
+OPTS="$OPTS -drive id=disk0,if=none,cache=unsafe,format=raw,file=/var/lib/libvirt/images/windows10pro.img"
 OPTS="$OPTS -device driver=virtio-scsi-pci,id=scsi0"
 OPTS="$OPTS -device scsi-hd,drive=disk0"
 
 
 # 1st Game drive
-OPTS="$OPTS -drive id=disk1,if=none,cache=none,aio=native,format=raw,file=/dev/disk/by-id/ata-Hitachi_HDS721050CLA660_JP1570FR1ZWP7K"
-OPTS="$OPTS -device driver=virtio-scsi-pci,id=scsi1"
-OPTS="$OPTS -device scsi-hd,drive=disk1"
+#OPTS="$OPTS -drive id=disk1,if=none,cache=none,aio=native,format=raw,file=/dev/disk/by-id/ata-Hitachi_HDS721050CLA660_JP1570FR1ZWP7K"
+#OPTS="$OPTS -device driver=virtio-scsi-pci,id=scsi1"
+#OPTS="$OPTS -device scsi-hd,drive=disk1"
 
 
 # 2nd Game drive
-OPTS="$OPTS -drive id=disk2,if=none,cache=none,aio=native,format=raw,file=/dev/disk/by-id/ata-Hitachi_HDS5C3020ALA632_ML0220F30NX2DD"
-OPTS="$OPTS -device driver=virtio-scsi-pci,id=scsi2"
-OPTS="$OPTS -device scsi-hd,drive=disk2"
+#OPTS="$OPTS -drive id=disk2,if=none,cache=none,aio=native,format=raw,file=/dev/disk/by-id/ata-Hitachi_HDS5C3020ALA632_ML0220F30NX2DD"
+#OPTS="$OPTS -device driver=virtio-scsi-pci,id=scsi2"
+#OPTS="$OPTS -device scsi-hd,drive=disk2"
 
 
 # Windows 10 Pro installer
-OPTS="$OPTS -drive id=cd0,if=none,format=raw,readonly,file=/data/iso/OS/Windows10Pro/Windows_10_Professional_64_Bit.iso"
+OPTS="$OPTS -drive id=cd0,if=none,format=raw,readonly,file=/home/sysop/Documents/Windows.iso"
 OPTS="$OPTS -device driver=ide-cd,bus=ide.0,drive=cd0"
 
 
 # Virtio driver
-OPTS="$OPTS -drive id=virtiocd,if=none,format=raw,file=/data/iso/VirtIO/virtio-win-0.1.105.iso"
+OPTS="$OPTS -drive id=virtiocd,if=none,format=raw,file=/home/sysop/Documents/virtio-win-0.1.160.iso"
 OPTS="$OPTS -device driver=ide-cd,bus=ide.1,drive=virtiocd"
 
 
@@ -114,8 +114,8 @@ OPTS="$OPTS -device virtio-net-pci,mac=52:54:00:xx:xx:xx,netdev=brlan"
 
 
 # Disable display
-#OPTS="$OPTS -vga qxl"
-OPTS="$OPTS -vga none"
+OPTS="$OPTS -vga qxl"
+#OPTS="$OPTS -vga none"
 OPTS="$OPTS -serial null"
 OPTS="$OPTS -parallel null"
 #OPTS="$OPTS -monitor none"
